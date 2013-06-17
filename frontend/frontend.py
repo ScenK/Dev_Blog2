8bc6b6d4d69e9b46fdcf0fe88e39ca2c04603946
ScenK <he.kang@dev-engine.com>
2013-06-17 14:44:57 +0800

    diary detail and url_pretty done

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -9,3 +9,12 @@ frontend = Blueprint('frontend', __name__, template_folder='templates', static_f
 def home():
     diaries = Diary.objects.order_by('-publish_time')
     return render_template('frontend/home.html', diaries=diaries)
+
+@frontend.route('/diary/<diary_id>/<diary_title>')
+def diary_detail(diary_id, diary_title=None):
+    try:
+        diary = Diary.objects(pk=diary_id)[0]
+    except:
+        abort(404)
+
+    return render_template('frontend/diary/detail.html', diary=diary)
