8997da4006841e9ae483ffdd6ca21ddbeb2a6959
ScenK <he.kang@dev-engine.com>
2013-07-23 11:53:25 +0800

    bugfix for tornado 404

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -4,4 +4,3 @@ document.write(unescape("%3Cscript src='/static/lib/googlecode/prettify.js' type
 
 document.write(unescape("%3Cscript src='/static/js/frontend/tools.js' type='text/javascript'%3E%3C/script%3E"));
 document.write(unescape("%3Cscript src='/static/js/frontend/global.js' type='text/javascript'%3E%3C/script%3E"));
-
